5cd2990f8997e7ab4fde684f9e80e0b1ffb28a01
BAOUCHbaSalma <155727770+BAOUCHbaSalma@users.noreply.github.com>
2024-02-16 16:10:12 +0100

    Creation du l'ajout du taille des poupees

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,38 @@
+import java.util.Scanner;
+import java.util.Scanner;
+
 public class Menu {
-}
+
+
+
+
+        public void listChoix(){
+            int sizeDoll = 0;
+
+
+            System.out.println("Entrer the size of the first Doll");
+            sizeDoll = new Scanner(System.in).nextInt();
+            PoupeeRusse doll1 = new PoupeeRusse(sizeDoll);
+
+
+
+
+
+            System.out.println("Entrer the size of the second Doll");
+            int sizeDoll2 = new Scanner(System.in).nextInt();
+            PoupeeRusse doll2 = new PoupeeRusse(sizeDoll2);
+
+
+            System.out.println("Entrer the size of the 3 Doll");
+            int sizeDoll3 = new Scanner(System.in).nextInt();
+            PoupeeRusse doll3 = new PoupeeRusse(sizeDoll3);
+
+
+
+//    System.out.println(doll1.getTaille());
+//    System.out.println(doll2.getTaille());
+//    System.out.println(doll3.getTaille());
+
+
+
+        }}
